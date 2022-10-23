45ef101ed46efb4bf9d90c607c954b632b6bdb4b
SaCode <69605234+EimaMei@users.noreply.github.com>
2022-10-23 17:59:44 +0300

    whitespaces fixed + .gitignore

--- main.cpp
+++ main.cpp
@@ -4,24 +4,24 @@
 #include <functions.hpp>
 
 int main(int argc, char** argv) {
-    std::string filename;
+	std::string filename;
 
 	if (argc <= 1) { std::cout << HCL::colorText("Error",  HCL::OUTPUT_RED) << ": No input files provided" << std::endl; return -1;  }
 	else {
 		for (int i = 1; i < argc; i++) {
 			std::string arg = (std::string)argv[i];
 			if (arg == "-debug" || arg == "-g") { 
-                HCL::debug = true; 
-                continue; 
-            }
+				HCL::debug = true; 
+				continue; 
+			}
 			else 
-                filename = arg; 
+				filename = arg; 
 		}
 	}
-    HCL::interpreteFile(filename);
+	HCL::interpreteFile(filename);
 
-    if (HCL::debug)
-        HCL::debugMode();
+	if (HCL::debug)
+		HCL::debugMode();
 
-    return 0;
+	return 0;
 }
